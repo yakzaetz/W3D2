8474d5d5a5e5ea5d5cfe177e71eb91ffaf6bbcca
Deanding <dingtian.ding@gmail.com>
2021-12-14 16:07:23 -0500

    4pm commit

--- board.rb
+++ board.rb
@@ -1,9 +1,22 @@
 require_relative "game.rb"
 require_relative "card.rb"
+require "byebug"
 
 
 class Board
-    def initialize
+    attr_reader :size
+    def initialize(n) #n = n*n
+        @grid = Array.new(n) {Array.new(n, :N)}
+        @size = n * n
+        @shuffled_array = Card.assign_value(n)
+    end
+
+    def populate 
         
+        (0...@grid.length).each do |row| 
+            (0...@grid.length).each do |col|
+                @grid[row][col] = Card.new(@shuffled_array.pop)
+            end
+        end
     end
 end
--- card.rb
+++ card.rb
@@ -2,27 +2,58 @@ require_relative "game.rb"
 require_relative "board.rb"
 
 
-class Card
+class Card  
 
-    def self.assign_value
-        random_letter = (("a".."z").to_a.sample.upcase)
-        pair = random_letter * 2
+    attr_reader 
+    attr_writer :position
+
+    def self.assign_value(n) #shuffle the array, return shuffled array
+        array=[]
+        while array.length < ( n * n )
+            random_letter = (("a".."z").to_a.sample.upcase)
+                if !array.include?(random_letter)
+                    2.times do
+                    array << random_letter
+                    end
+                end
+        end 
+        array.shuffle
     end
 
-    def initialize(value, face_down)
-        @value = Card.assign_value
-        @position = face_down
-        @pairs = []
+    def initialize(value)
+        @value = value
+        @hidden = true #boolean #hidden/revealed
+        # @pairs = []
     end
+
+    def hide
+        @hidden = true
+
+    end
+
+    def reveal 
+        @hidden = false
+    end
+
+    # def to_s
+
+    # end 
+
+    # def ==
+
+    # end
 
 end
+# .shuffle on an array
 
-if board[][].empty?
-card = Card.assign_value.split("")
-rand_row = rand(0...board.length)
-rand_col = rand(0...board.length)
-pos = rand_row, rand_col
-card.each do |c|
-    @board.[](pos, c)
-        self[pos] = c
-    end 
+# if board[][].empty?
+# card = Card.assign_value.split("")
+
+# rand_row = rand(0...board.length)
+# rand_col = rand(0...board.length)
+# pos = rand_row, rand_col
+# card.each do |c|
+#     @board.[](pos, c)
+#         self[pos] = c
+#     end 
+
